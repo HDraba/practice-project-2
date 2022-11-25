9fe13f9012b843cd68e67530cca730c81cfb0895
Hannes Draba <H.Draba@porta.de>
2022-11-25 10:23:03 +0100

    added basic validation

--- src/components/Cart/Checkout.tsx
+++ src/components/Cart/Checkout.tsx
@@ -5,48 +5,95 @@ type CheckoutProps = {
   onClose: () => void;
 };
 
+const isEmpty = (value: string) => value.trim() === '';
+const isFiveChars = (value: string) => value.trim().length === 5;
+
 const Checkout = (props: CheckoutProps) => {
-  const nameRef = useRef<HTMLInputElement>(null)
-  const streetRef = useRef<HTMLInputElement>(null)
-  const postRef = useRef<HTMLInputElement>(null)
-  const cityRef = useRef<HTMLInputElement>(null)
-
   
-  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    const [nameIsValid, setNameIsValid] = useState<boolean>(false)
-    
-    event.preventDefault()
-    
-    console.log(nameRef.current!.value) 
+  // set them as valid to not get any errors at start 
+  // comment out for reducer
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    post: true,
+    city: true,
+  });
 
-    props.onClose()
+  const nameRef = useRef<HTMLInputElement>(null);
+  const streetRef = useRef<HTMLInputElement>(null);
+  const postRef = useRef<HTMLInputElement>(null);
+  const cityRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const enteredName = nameRef.current!.value;
+    const enteredStreet = streetRef.current!.value;
+    const enteredPost = postRef.current!.value;
+    const enteredCity = cityRef.current!.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostIsValid = !isEmpty(enteredPost);
+    const enteredCityIsValid =
+      isFiveChars(enteredCity) && !isEmpty(enteredCity);
+
+    // comment out for reducer 
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      post: enteredPostIsValid,
+      city: enteredCityIsValid
+    })
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostIsValid &&
+      enteredCityIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+    props.onClose();
+
+    console.log(nameRef.current!.value);
   };
 
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div className={` ${styles.control} ${formInputsValidity.name ? '' : styles.invalid}`}>
+        <label htmlFor="name">Your Name</label>
+        <input ref={nameRef} type="text" id="name" />
+        {!formInputsValidity.name && <p>Enter a valid name</p>}
+      </div>
+      <div className={` ${styles.control} ${formInputsValidity.street ? '' : styles.invalid}`}>
 
-  return (  <form className={styles.form} onSubmit={submitHandler}>
-    <div className={styles.control}>
-      <label htmlFor='name'>Your Name</label>
-      <input ref={nameRef} type='text' id='name' />
-    </div>
-    <div className={styles.control}>
-      <label htmlFor='street'>Street</label>
-      <input ref={streetRef} type='text' id='street' />
-    </div>
-    <div className={styles.control}>
-      <label htmlFor='post'>Postcode</label>
-      <input ref={postRef} type='text' id='post' />
-    </div>
-    <div className={styles.control}>
-      <label htmlFor='city'>City</label>
-      <input ref={cityRef} type='text' id='city' />
-    </div>
-    <div className={styles.actions}>
-      <button type='button' onClick={props.onClose}>
-        Cancel
-      </button>
-      <button className={styles.submit}>Confirm</button>
-    </div>
-  </form>
+        <label htmlFor="street">Street</label>
+        <input ref={streetRef} type="text" id="street" />
+        {!formInputsValidity.street && <p>Enter a valid street</p>}
+      </div>
+      <div className={` ${styles.control} ${formInputsValidity.post ? '' : styles.invalid}`}>
+
+        <label htmlFor="post">Postcode</label>
+        <input ref={postRef} type="text" id="post" />
+        {!formInputsValidity.post && <p>Enter a valid postcode</p>}
+
+      </div>
+      <div className={` ${styles.control} ${formInputsValidity.city ? '' : styles.invalid}`}>
+
+        <label htmlFor="city">City</label>
+        <input ref={cityRef} type="text" id="city" />
+        {!formInputsValidity.city && <p>Enter a valid city</p>}
+
+      </div>
+    .  <div className={styles.actions}>
+        <button type="button" onClick={props.onClose}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
+      </div>
+    </form>
   );
 };
 
